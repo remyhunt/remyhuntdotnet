f5f0b8b8903e48393d0af137128465c955c083d6
Ashton Hunter <Ashton Hunter>
2017-08-28 05:21:30 +0000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 80)
+        }, 1220, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+ 
+
+})(jQuery); // End of use strict
