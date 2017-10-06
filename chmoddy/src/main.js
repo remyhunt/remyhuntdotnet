272883c872359a497cf0d7bf9f3a7a4e7af28c6c
Rem Hunter <ashtonhunter3@gmail.com>
2017-10-05 21:47:47 -0700

    added node modules

--- chmoddy/src/main.js
+++ chmoddy/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Bootstrap from '.././node_modules/bootstrap-css-only'
+import Bootstrap from '../../node_modules/bootstrap-css-only'
 import App from './App.vue'
 
 import 'bootstrap-css-only/css/bootstrap.min.css'
