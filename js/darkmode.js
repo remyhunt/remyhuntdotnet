14d43bc53a7ce7d97736d572e800f11b8fa43766
Remy Hunter <remyhunter256@gmail.com>
2020-05-17 12:36:55 -0400

    Add more sophisticated dark mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,10 +1,35 @@
 var toggle = document.getElementById('toggle');
 var html = document.getElementsByTagName('html')[0];
+var darkModeLabel = document.getElementsByClassName('dark-mode-label')[0];
+darkModeLabel.innerHTML = '🌙 psst? dark mode? '
+var isDark = false;
+
+var today = new Date();
+var hours = today.getHours();
+
+console.log(hours);
+
+// window.setTimeOut = (alert('hi'), 5000);
+setTimeout(function() {
+    hours > 11 && hours < 13 ? setDarkMode(true) : setDarkMode(false);
+}, 1000);
+
+
+
 toggle.addEventListener('change', e => {
-	if (e.target.checked) {
-    html.classList.add('darkmode');
-    console.log('got here');
-	} else {
-		html.classList.remove('darkmode');
-	}
+	e.target.checked ? setDarkMode(true): setDarkMode(false); 
 });
+
+
+function setDarkMode(isDark){
+	if(isDark) {
+		html.classList.add('darkmode');
+		toggle.checked = true;
+		darkModeLabel.innerHTML = '💡 turn the lights back on?';
+	} 
+	else {
+		html.classList.remove('darkmode');
+		toggle.checked = false;
+		darkModeLabel.innerHTML = '🌙 psst? dark mode? '
+	}
+}
