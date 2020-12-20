b1845f4686680d90e0bb3c92dc78c5042cae98bc
remy <remyhunter256@gmail.com>
2020-12-20 08:33:14 -0500

    Add session variable to persist Dark Mode across pages

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,57 +1,72 @@
 var toggle = document.getElementById('toggle');
 var html = document.getElementsByTagName('html')[0];
 // var darkModeLabel = document.getElementsByClassName('dark-mode-label')[0];
-
 // darkModeLabel.innerHTML = '🌙 psst? dark mode? ';
 
 var today = new Date();
 var hours = today.getHours();
 
 
-// How dark mode is initiailly determined 
-setTimeout(function() {
-	if(window.matchMedia) {
-		if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-			setDarkMode(true, '(dark mode detected) 💡 lights on?');
-		} else {
+
+if(sessionStorage.getItem('isDarkMode') != null) {
+	if(sessionStorage.getItem('isDarkMode') == 'true') {
+			setDarkMode(true);
+		}else {
 			setDarkMode(false);
 		}
-	} 
-	else {
-		if(hours > 20 || hours < 8){ 
-			setDarkMode(true) 
-		} 
-		else { 
-			setDarkMode(false);
-		};
+	} else {
+		setTimeout(function() {
+			if(window.matchMedia) {
+
+				if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+					setDarkMode(true);
+				} else {
+					setDarkMode(false);
+				}
+			} 
+			else {
+				if(hours > 20 || hours < 8){ 
+					setDarkMode(true) 
+				} 
+				else { 
+					setDarkMode(false);
+				};
+			}
+		}, 800);
 	}
-}, 800);
+
 
 // Listener for a change in checkbox
 toggle.addEventListener('change', (e) => {
-	e.target.checked ? setDarkMode(true): setDarkMode(false); 
+	e.target.checked ? setDarkMode(true, true): setDarkMode(false); 
 });
 
 
-// Listener for a change in Media Query (from system UI)
+// // Listener for a change in Media Query (from system UI)
 window.matchMedia('(prefers-color-scheme: dark)').addListener( (e) => {
 	e.matches ? setDarkMode(true) : setDarkMode(false);
 });
 
 
-function setDarkMode(isDark, customMessage){
+
+
+
+function setDarkMode(isDark, transition){
 	if(isDark == true) {
-		html.classList.add('darkmode');
+		html.classList.add('darkmode'); 
+		if(transition) {
+			html.classList.add('darkmode-transition'); 
+		}
+		sessionStorage.setItem('isDarkMode', 'true');
+		
 		toggle.checked = true;
 		// darkModeLabel.innerHTML = '💡 turn the lights back on?';
 	} 
 	else {
+		sessionStorage.setItem('isDarkMode', 'false');
 		html.classList.remove('darkmode');
 		toggle.checked = false;
 		// darkModeLabel.innerHTML = '🌙 psst? dark mode? '
 	}
 	
-	if(customMessage) {
-		// darkModeLabel.innerHTML = customMessage;
-	}
 }
